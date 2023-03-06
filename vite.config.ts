7188bc56dbb16c621aadb0eb9e713c08358049bc
spiritbroo <rinoakbr@gmail.com>
2023-03-06 20:45:18 +0700

    da

--- vite.config.ts
+++ vite.config.ts
@@ -79,6 +79,9 @@ export default defineConfig(() => {
                 rollupNodePolyFill(),
                 ],
           },
-      }
+      },
+      define: {
+        'process.env': {}
+    }
   };
 });
