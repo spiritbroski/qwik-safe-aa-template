590401646abc1a0578bd7cdfddcec0b2356bb827
spiritbroo <rinoakbr@gmail.com>
2023-03-08 00:04:27 +0700

    da

--- src/entry.express.tsx
+++ src/entry.express.tsx
@@ -14,7 +14,7 @@ import render from './entry.ssr';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { join } from 'node:path';
-
+import cors from 'cors'
 declare global {
   interface QwikCityPlatform extends PlatformNode {}
 }
