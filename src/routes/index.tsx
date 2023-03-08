3a929620191ca0f7b43ac9150b708301e4d319be
spiritbroo <rinoakbr@gmail.com>
2023-03-08 09:10:46 +0700

    da

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,27 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 import { MUIButton  } from '~/integrations/react/mui';
 import { SafeAuthKit, SafeAuthProviderType } from '@spiritbro1/auth-kit'
 import {action$} from '@builder.io/qwik-city';
+import type { RequestHandler } from '@builder.io/qwik-city';
 export const useAddUser = action$(() => {
     console.log("sda")
     return {
         success: true
     };
 });
+export const onRequest:RequestHandler = ({ headers, query,method,json,status }) => {
+    headers.set('Access-Control-Allow-Credentials', "true")
+    headers.set('Access-Control-Allow-Origin', '*')
+    headers.set('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    headers.set(
+            'Access-Control-Allow-Headers',
+            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+            )
+    if (method === 'OPTIONS') {
+        status(200)
+        return
+    }
+   
+};
 export async function panda(){
 
       const safeAuthKit = await SafeAuthKit.init(SafeAuthProviderType.Web3Auth, {
